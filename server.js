114da779efd5502e9a21117295496c9b390ef331
Kavipragash95@gmail.com <Kavipragash95@gmail.com>
2025-06-05 09:39:00 +0530

    Bug Fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-import express, { json } from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 const app = express();
-import { sync } from "./config/database";
+const sequelize = require("./config/database");
 
 
 // Middleware
-app.use(json());
+app.use(express.json());
 app.use(cors());
 
 // Routes
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-sync({ alter: true }) // Updates tables without dropping data
+sequelize
+  .sync({ alter: true }) // Updates tables without dropping data
   .then(() => console.log("✅ Database & tables synced successfully!"))
   .catch((err) => console.error("❌ Error syncing database:", err));
 
